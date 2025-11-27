d0a86108b67661f9372921136d9bd1acfaa68e9f
superneo1020 <supertsang1020@gmail.com>
2025-11-27 19:12:52 +0800

    Frontend update: new features for users table

---
+++ springbackend/src/main/java/com/eduquest/springbackend/dao/UserRepository.java
@@ -0,0 +1,8 @@
+package com.eduquest.springbackend.dao;
+
+import com.eduquest.springbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsernameAndPassword(String username, String password);
+}
